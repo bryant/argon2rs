87caa8849abb590205e413718a057b4110bd3e3b
bryant <bryant@defrag.in>
2016-02-04 16:34:10 -0500

    update hello world call to convenience wrapper

--- examples/helloworld.rs
+++ examples/helloworld.rs
@@ -3,7 +3,7 @@ extern crate argon2rs;
 pub fn main() {
     let (password, salt) = ("argon2i!", "delicious salt");
     println!("argon2i(\"argon2i\", \"delicious\"):");
-    for byte in argon2rs::simple2i(&password, &salt).iter() {
+    for byte in argon2rs::argon2i_simple(&password, &salt).iter() {
         print!("{:02x}", byte);
     }
     println!("");
