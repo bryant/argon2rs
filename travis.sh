2ed4cadce2bca8e7e785d4333d6d3d2004871367
bryant <bryant@defrag.in>
2016-05-11 08:10:36 -0400

    travis: tests on stable, tests and benches on nightly

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
 cargo build
-# tests use `extern crate test` which requires nightly.
+cargo test
+# benches use `extern crate test` which requires nightly.
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]
 then
-    cargo test
+    cargo bench
 fi
