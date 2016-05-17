840e0b41d949e7380c7c5cdffa206d493ec4209a
bryant <bryant@defrag.in>
2016-05-17 16:12:34 +0000

    rename `constant_eq` tests to more meaningful

--- benches/constant_eq.rs
+++ benches/constant_eq.rs
@@ -7,13 +7,13 @@ extern crate argon2rs;
 use argon2rs::verifier::constant_eq;
 
 #[bench]
-fn test_constant_eq0(b: &mut test::Bencher) {
+fn param_are_equal(b: &mut test::Bencher) {
     let lhs = (0..255).cycle().take(9001).collect::<Vec<u8>>();
     b.iter(|| constant_eq(&lhs[..], &lhs[..]));
 }
 
 #[bench]
-fn test_constant_eq1(b: &mut test::Bencher) {
+fn params_are_unequal(b: &mut test::Bencher) {
     let lhs = (0..255).cycle().take(9001).collect::<Vec<u8>>();
     let mut rhs = lhs.clone();
     rhs[0] += 24;
