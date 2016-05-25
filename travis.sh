3b94045100b5c177d88819dfd988d10c8daaa7cf
bryant <bryant@defrag.in>
2016-05-25 05:23:40 +0000

    travis: bench with and without avx (if present)

--- travis.sh
+++ travis.sh
@@ -3,5 +3,14 @@ cargo test
 # benches use `extern crate test` which requires nightly.
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]
 then
-    cargo bench
+    if [ ! -z "`grep '\<avx\>' /proc/cpuinfo`" ]
+    then
+        echo "=== benching with '-C target-feature=+avx' ==="
+        export RUSTFLAGS='-C target-feature=+avx'
+        cargo clean && cargo bench --features simd
+    fi
+
+    unset RUSTFLAGS
+    echo "=== benching without avx ==="
+    cargo clean && cargo bench --features simd
 fi
