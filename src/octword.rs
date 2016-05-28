7650c5c90a307a2b817717884ac248e8fb85fcf9
Xidorn Quan <me@upsuper.org>
2016-05-28 14:17:19 +1000

    workaround compiler asserts
    fix #10

--- src/octword.rs
+++ src/octword.rs
@@ -160,22 +160,27 @@ impl u64x2 {
 mod test {
     use super::u64x2;
 
-    const T0: u64x2 = u64x2(0xdeadbeef_01234567, 0xcafe3210_babe9932);
-    const T1: u64x2 = u64x2(0x09990578_01234567, 0x1128f9a9_88e89448);
+    // XXX Use function call to workaround rust-lang/rust#33764.
+    fn t0() -> u64x2 {
+        u64x2(0xdeadbeef_01234567, 0xcafe3210_babe9932)
+    }
+    fn t1() -> u64x2 {
+        u64x2(0x09990578_01234567, 0x1128f9a9_88e89448)
+    }
 
     #[test]
     fn test_rotate_right() {
         for &s in [8 as u32, 16, 24, 32].iter() {
-            assert_eq!(T0.rotate_right(s).0, T0.0.rotate_right(s));
-            assert_eq!(T0.rotate_right(s).1, T0.1.rotate_right(s));
+            assert_eq!(t0().rotate_right(s).0, t0().0.rotate_right(s));
+            assert_eq!(t0().rotate_right(s).1, t0().1.rotate_right(s));
         }
     }
 
     #[test]
     fn test_lower_mult() {
         use super::lo;
-        assert_eq!(T0.lower_mult(T1), T1.lower_mult(T0));
-        assert_eq!(T0.lower_mult(T1).0, lo(T0.0) * lo(T1.0));
-        assert_eq!(T0.lower_mult(T1).1, lo(T0.1) * lo(T1.1));
+        assert_eq!(t0().lower_mult(t1()), t1().lower_mult(t0()));
+        assert_eq!(t0().lower_mult(t1()).0, lo(t0().0) * lo(t1().0));
+        assert_eq!(t0().lower_mult(t1()).1, lo(t0().1) * lo(t1().1));
     }
 }
